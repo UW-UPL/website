db1da9c8f79d458ac6dc4289de460600f7b4ee07
Michael Gira <michael@gira.dev>
2022-03-20 23:13:00 -0500

    Add typings for Gatsby prefix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
+declare const __PATH_PREFIX__: string;
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`;
   const isRootPath = location.pathname === rootPath;
