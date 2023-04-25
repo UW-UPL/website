e1c85aa4a51c59ee8951f81fc813a65becce5c3f
sbaumohl <sbaumohl@wisc.edu>
2023-04-25 14:52:36 -0500

    staggered hours, headers, and description

--- src/pages/hours.tsx
+++ src/pages/hours.tsx
@@ -11,7 +11,8 @@ import {
   Tbody,
   Td,
   Text,
-  Divider
+  Divider,
+  Heading,
 } from "@chakra-ui/react";
 
 const HoursIndex = ({ data, location }) => {
@@ -41,12 +42,15 @@ const HoursIndex = ({ data, location }) => {
 
   return (
     <Layout location={location}>
+      <Heading textAlign="center" as="h1" mb="7">
+        Hours
+      </Heading>
       <Text w="60%" textAlign="left" ml="auto" mr="auto" mb="8" fontSize="lg">
-        Anyone is welcome in the UPL room whenever a coord is present, and coords
-        commit to be present at certain office hours. All you need to do is come
-        in and say hi!{" "}
+        Anyone is welcome in the UPL room whenever a coord is present, and
+        coords commit to be present at certain office hours. All you need to do
+        is come in and say hi!{" "}
       </Text>
-      <Divider mb="4" w="60%" ml="auto" mr="auto"/>
+      <Divider mb="4" w="60%" ml="auto" mr="auto" />
       <TableContainer w="65%" mr="auto" ml="auto" borderRadius="md">
         <Table variant="striped" colorScheme="red">
           <Thead>
@@ -59,13 +63,24 @@ const HoursIndex = ({ data, location }) => {
             </Tr>
           </Thead>
           <Tbody>
+            <Tr>
+              {headers.map(() => (
+                <Td> </Td>
+              ))}
+            </Tr>
             {rows.map((r) => (
               <Tr>
-                {r.map((e) => (
-                  <Td fontSize="lg" color="gray.700" bg="gray.50">
-                    {e}
-                  </Td>
-                ))}
+                {r.map((e, index) => {
+                  return index === 0 ? (
+                    <Td fontSize="lg" color="gray.700" bg="gray.50" pt="4">
+                      <Text mt="-9">{e}</Text>
+                    </Td>
+                  ) : (
+                    <Td fontSize="lg" color="gray.700" bg="gray.50" pt="4">
+                      {e}
+                    </Td>
+                  );
+                })}
               </Tr>
             ))}
           </Tbody>
