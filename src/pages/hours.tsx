1f785b564e2a97a713faa12cf8570a20ab0f4384
sbaumohl <sbaumohl@wisc.edu>
2023-04-25 11:39:40 -0500

    added styling to hours page

--- src/pages/hours.tsx
+++ src/pages/hours.tsx
@@ -2,6 +2,17 @@ import * as React from "react";
 import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
+import {
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Text,
+  Divider
+} from "@chakra-ui/react";
 
 const HoursIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`;
@@ -19,29 +30,47 @@ const HoursIndex = ({ data, location }) => {
     }
   }
 
+  let headers = [
+    "Time",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ];
+
   return (
     <Layout location={location}>
-      <table>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Monday</th>
-            <th>Tuesday</th>
-            <th>Wednesday</th>
-            <th>Thursday</th>
-            <th>Friday</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((r) => (
-            <tr>
-              {r.map((e) => (
-                <td>{e}</td>
+      <Text w="60%" textAlign="left" ml="auto" mr="auto" mb="8" fontSize="lg">
+        Anyone is welcome in the UPL room whenever a coord is present, and coords
+        commit to be present at certain office hours. All you need to do is come
+        in and say hi!{" "}
+      </Text>
+      <Divider mb="4" w="60%" ml="auto" mr="auto"/>
+      <TableContainer w="65%" mr="auto" ml="auto" borderRadius="md">
+        <Table variant="striped" colorScheme="red">
+          <Thead>
+            <Tr>
+              {headers.map((e) => (
+                <Th bgColor="gray.100" color="gray.700" fontSize="md">
+                  {e}
+                </Th>
               ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {rows.map((r) => (
+              <Tr>
+                {r.map((e) => (
+                  <Td fontSize="lg" color="gray.700" bg="gray.50">
+                    {e}
+                  </Td>
+                ))}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Layout>
   );
 };
